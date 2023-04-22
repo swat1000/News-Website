bf2fa64e3f50ca5851c7a1fd79ed9ad9a62f1602
Swatantra chaurasia <chaurasiyaswatantra65@gmail.com>
2023-04-22 20:02:58 +0530

    search bar implementation done

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
-let p = fetch("https://newsapi.org/v2/everything?q=bitcoin&apiKey=93aa5422144741d5bf86695b209c5011")
-p.then((value1) => {
-    // console.log(value1.status)
-    // console.log(value1.ok)
-    return value1.json()
-}).then((value2) => {
-    console.log(value2.articles[0]);
-    for (i = 0; i < value2.articles.length; i++) {
-        console.log(i)
-        var html = `<div class="card m-4" style="width: 18rem;">
-        <img src="${value2.articles[i].urlToImage}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${value2.articles[i].title}</h5>
-            <p class="card-text">${value2.articles[i].content}</p>  
-            <p class="card-text">Author: ${value2.articles[i].author} <br> Published on: ${value2.articles[i].publishedAt} <br> Source: ${value2.articles[i].source.name} </p>
-            <a href="${value2.articles[i].url}" class="btn btn-primary">Read More</a>
-        </div>
-    </div>`;
+getNews();
 
-        $(".container").append(html);
+function getNews (){
+    $(".container").text("");
+
+    var searchkey = $("#keyword").val();
+
+    if(searchkey == ""){
+        searchkey = "latest";
     }
-}) 
+
+    let p = fetch("https://newsapi.org/v2/everything?q="+searchkey+"&apiKey=93aa5422144741d5bf86695b209c5011")
+    p.then((value1) => {
+        // console.log(value1.status)
+        // console.log(value1.ok)
+        return value1.json()
+    }).then((value2) => {
+        console.log(value2.articles[0]);
+        for (i = 0; i < value2.articles.length; i++) {
+            console.log(i)
+            if (value2.articles[i].urlToImage == null){
+                
+                value2.articles[i].urlToImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/No-Image-Placeholder.svg/1200px-No-Image-Placeholder.svg.png";
+            }
+            var html = `<div class="card m-2 p-2" style="width: 100%;">
+            <div class="row g-4">
+              <div class="col-md-4">
+                <img src="${value2.articles[i].urlToImage}" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">${value2.articles[i].title}</h5>
+                  <p class="card-text">${value2.articles[i].content}</p>
+                  <p class="card-text">Author: ${value2.articles[i].author} <br> 
+                  Source: ${value2.articles[i].source.name}<br> 
+                  <small class="text-body-secondary">Published On: ${value2.articles[i].publishedAt}</small></p>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    
+            $(".container").append(html);
+        }
+    }) 
+}
+
+
